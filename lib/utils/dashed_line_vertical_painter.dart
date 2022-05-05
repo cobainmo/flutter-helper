6f53cc18274d80380e2c89a24f50cbbf0ee468f7
cobainmo <mensur.djogic@gmail.com>
2022-05-05 16:34:59 +0200

    added Dashed line vertical painter helper

---
+++ lib/utils/dashed_line_vertical_painter.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class DashedLineVerticalPainter extends CustomPainter {
+  final Color color;
+  DashedLineVerticalPainter({required this.color});
+  @override
+  void paint(Canvas canvas, Size size) {
+    double dashHeight = 5, dashSpace = 3, startY = 0;
+    final paint = Paint()
+      ..color = color
+      ..strokeWidth = 1;
+    while (startY < size.height) {
+      canvas.drawLine(Offset(0, startY), Offset(0, startY + dashHeight), paint);
+      startY += dashHeight + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
