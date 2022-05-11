9148e1ef02665bccdf1ad5a32783b477125ea2d2
cobainmo <mensur.djogic@gmail.com>
2022-05-11 21:42:23 +0200

    Added maxSize constraints argument

--- lib/UI/progress_dialog.dart
+++ lib/UI/progress_dialog.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
 Future<void> showProgressDialog(BuildContext context, Future future,
-    {bool dismissible = false, Widget? content, String? message = 'Please wait ...', ShapeBorder? dialogShape}) async {
+    {bool dismissible = false,
+    double? maxWidth,
+    Widget? content,
+    String? message = 'Please wait ...',
+    ShapeBorder? dialogShape}) async {
   await showDialog(
     context: context,
-    builder: (context) =>
-        ProgressDialog(
-          future,
-          content: content,
-          message: message,
-          shape: dialogShape,
-        ),
+    builder: (context) => ProgressDialog(
+      future,
+      content: content,
+      message: message,
+      maxWidth: maxWidth,
+      shape: dialogShape,
+    ),
     barrierDismissible: dismissible,
   );
 }
@@ -21,12 +25,17 @@ class ProgressDialog extends StatefulWidget {
 
   final Widget? content;
   final String? message;
+  final double? maxWidth;
 
   final ShapeBorder? shape;
   final EdgeInsets insetPadding;
 
-  ProgressDialog(this.future, {this.message, this.content, this.shape, this.insetPadding = const EdgeInsets.symmetric(
-      horizontal: 40.0, vertical: 24.0)});
+  ProgressDialog(this.future,
+      {this.message,
+      this.content,
+      this.shape,
+      this.maxWidth,
+      this.insetPadding = const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0)});
 
   @override
   State<StatefulWidget> createState() => ProgressDialogState();
@@ -63,16 +72,19 @@ class ProgressDialogState extends State<ProgressDialog> {
 
     Widget content = widget.content != null
         ? widget.content!
-        : Center(
-      heightFactor: 1.0,
-      child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Row(mainAxisSize: MainAxisSize.min, children: [
-            CircularProgressIndicator(),
-            const Padding(padding: const EdgeInsets.only(right: 10.0),),
-            if (widget.message != null) Expanded(child: Text(widget.message!)),
-          ])),
-    );
+        : Container(
+            constraints:
+                widget.maxWidth != null ? BoxConstraints(maxWidth: widget.maxWidth!) : BoxConstraints.tightForFinite(),
+            child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(mainAxisSize: MainAxisSize.min, children: [
+                  CircularProgressIndicator(),
+                  const Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                  ),
+                  if (widget.message != null) Expanded(child: Text(widget.message!)),
+                ])),
+          );
 
     return Dialog(child: content, shape: shape, insetPadding: widget.insetPadding);
   }
